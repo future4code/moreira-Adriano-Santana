fe7f7bf3be844353feccae98da8d862b8f7a6cb7
Britobtx <britobtx@gmail.com>
2021-11-30 18:32:43 -0300

    exercicio resolvido

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,51 @@
+/* 
+1 interpretação
+let a = 10
+let b = 10
+
+console.log(b) iguál a 10
+
+b = 5
+console.log(a, b) a igual á 10 e b gual á 5   */
+
+/* 
+2ª interpretação 
+let a = 10
+let b = 20
+c = a
+b = c
+a = b
+
+console.log(a, b, c)  10 10 10 */
+
+
+/* 3ª interpretação
+
+let horasTrabalhadas = prompt("Quantas horas você trabalha por dia?")
+let salarioDiaria = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${t/p} por hora`) */
+
+
+/* //Exercícios de escrita de código 1
+
+const nome = prompt("Qual o seu nome ?")
+const idade = prompt("Qual a sua idade ?")
+
+
+console.log (typeof nome,typeof idade)
+// resuldado string string
+
+console.log ("Olá",nome," sua idade é de",idade ,"anos") */
+// 2ª 
+const tempo = prompt("vai chover  hoje ?") 
+const guardaChuva = prompt("Voce tem guarda-chuva ?")
+const situação = prompt("e se chover vai se molhar ?")
+
+const respostaTempo = true
+const respostaGuardachuva = false
+const respostaSituação = true
+
+console.log ( "vai chover  hoje ?",respostaTempo)
+console.log ( "vai chover  hoje ?",respostaGuardachuva)
+console.log ( "vai chover  hoje ?",respostaSituação)
+

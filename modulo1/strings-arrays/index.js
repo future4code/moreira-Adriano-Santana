a6288738c359df3c0e1d12b04712e26fd9fd4d30
Britobtx <britobtx@gmail.com>
2021-12-01 16:59:53 -0300

    strings

---
+++ modulo1/strings-arrays/index.js
@@ -0,0 +1,48 @@
+//  Exercício de fixação
+
+/*  let array
+console.log('a. ', array) */ // array indefinida
+
+/* array = null    //  nulo,vazio
+console.log('b. ', array) */
+
+/* array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+console.log('c. ', array.length) */ // quantidade de itens 11 */
+
+/* let i = 0
+console.log('d. ', array[i]) */ // ARRAY INDICE 0 É 3
+
+ /* array[i + 1] = 19
+console.log('e. ', array)  */ // ARRAY 1 FOI SUBSTITUIDA POR 19
+
+/* const valor = array[i + 6] // array indice 0 = posição indice 6  = 9
+console.log('f. ', valor) */
+
+
+// exercicio 2
+/*  const frase = prompt("Digite uma frase")
+
+console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length)  */ /* a frese no consolo ficará em maiusculo
+e todas as vogais  'A' serão substituidas por 'I'  com 27 espaços */
+
+
+// Exercicio escrita de código
+
+// 1.
+/* const nomeDousuario = prompt("Qual o seu nome ?")
+const emailDousuario = prompt("Qual o seu e-mail?")
+
+console.log(`O e-mail ${emailDousuario} foi cadastrado com sucesso. Seja bem-vinda(o), ${nomeDousuario}!`) */
+
+///*  2.
+const comidaPreferida= ["sushi","churraco","feijoada","chocolate","sorvete"]
+console.log(comidaPreferida) 
+console.log("Essas são as minhas comidas preferidas:")
+
+console.log(comidaPreferida[0])
+console.log(comidaPreferida[1])
+console.log(comidaPreferida[2])
+console.log(comidaPreferida[3])
+console.log(comidaPreferida[4]) 
+
+const usuarioComida

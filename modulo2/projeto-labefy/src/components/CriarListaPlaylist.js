f4fff04fbfe7fabcc4af3a29d9eb8d4d9ee22db3
Britobtx <britobtx@gmail.com>
2022-01-27 20:55:39 -0300

    labefy

---
+++ modulo2/projeto-labefy/src/components/CriarListaPlaylist.js
@@ -0,0 +1,43 @@
+import React from "react";
+import axios from "axios";
+
+export default class CriarListaPlaylis extends React.Component {
+  state = {
+    nomeDaPlaylist: "",
+  };
+     pegarNomeDaPlaylist = (event) => {
+    this.setState({nome: event.target.value})
+}
+
+  CriarPlaylist = () => {
+    const urlCriaPlaylist = "https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists"
+
+    const body = {
+        "name": this.state.nomeDaPlaylist
+    }
+    const autorization = {
+      headers: {
+        Authorization: "adriano-brito-moreira"
+      }
+    }
+
+    axios.post(urlCriaPlaylist, body, autorization)
+    .then((response) => {
+      this.setState({nomeDaPlaylist:""})
+      alert("Playlist Criada com sucesso!")
+
+    })
+    .catch((err) => {alert(err)})
+  }
+  render(){
+      return(
+      <div>
+          <input 
+                value={this.state.nomeDaPlaylist} 
+                onChange={this.pegarNomeDaPlaylist} 
+                placeholder="playlist"
+              />
+              
+      </div>)
+  }
+}

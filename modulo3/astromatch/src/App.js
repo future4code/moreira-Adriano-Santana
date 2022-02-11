584b2590f0c64c2f7e03a0c3ce9a2e8834a31325
Britobtx <britobtx@gmail.com>
2022-02-11 20:38:59 -0300

    atualizando

--- modulo3/astromatch/src/App.js
+++ modulo3/astromatch/src/App.js
@@ -1,13 +1,64 @@
-import React from 'react';
-import {Perfil} from './components/styled';
+import React from "react";
+import { Perfil, Container } from "./components/styled";
+import { AiFillHeart } from "react-icons/ai";
+import { BsHandThumbsDownFill } from "react-icons/bs";
+import styled from "styled-components";
+import Logo from "./assets/img/logo.png";
 
+
+
+const UnMatch = styled.button`
+  font-size: 30px;
+  border: 1px solid transparent;
+  margin-top: 430px;
+  margin-left:10px;
+  width: 50px;
+  height: 50px;
+  display: flex;
+  
+  align-items: center;
+
+  :hover {
+    background-color: transparent;
+    color: blue;
+    transform: scale(2.5);
+  }
+`;
+const LogoAstroMatch = styled.img`
+  display: flex;
+  width: 180px;
+  height: 180px;
+  margin-left: 65px;
+  margin-top:10px;
+`;
+const Match = styled.button`
+  font-size: 30px;
+  border: 1px solid transparent;
+  width: 50px;
+  margin-top: 430px;
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  :hover {
+    background-color: transparent;
+    color: red;
+    transform: scale(2.5);
+  }
+`;
 
 export default function App() {
-  return(   
-    <div>
-      <Perfil/>
-    </div>)
-  
+  return (
+    <Container>
+      <Perfil>
+        <LogoAstroMatch src={Logo} />
+        <UnMatch>
+          <BsHandThumbsDownFill />
+        </UnMatch>
+        <Match>
+          <AiFillHeart />
+        </Match>
+      </Perfil>
+    </Container>
+  );
 }
-
-
---
+++ modulo3/astromatch/src/components/Botoes.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+import { AiFillHeart } from "react-icons/ai";
+import { BsHandThumbsDownFill } from "react-icons/bs";
+
+
+
+export default function Botoes() {
+  return (
+    <div>
+      <AiFillHeart />
+    </div>
+  );
+}

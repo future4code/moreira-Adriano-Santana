27538b4f681c68a51395b38a74cb7a185eca6836
Britobtx <britobtx@gmail.com>
2022-04-14 18:15:43 -0300

    Projeto Labecommerce

---
+++ modulo4 back-end/projeto labecommerce/src/types.ts
@@ -0,0 +1,12 @@
+export type user = {
+   id: number
+   name: string
+   email: string
+   password: string
+}
+export type products = {
+   id: string
+   name: string
+   price: number
+   image_url: string
+}

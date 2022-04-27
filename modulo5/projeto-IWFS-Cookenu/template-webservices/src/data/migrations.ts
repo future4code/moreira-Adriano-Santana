63ae7bd2f67f4446b03079ce1d2293a25bc8689a
adriano brito <adrianobrito3@hotmail.com>
2022-04-27 16:18:10 -0300

    cokenu

---
+++ modulo5/projeto-IWFS-Cookenu/template-webservices/src/data/migrations.ts
@@ -0,0 +1,24 @@
+import { connection } from "./connection"
+
+
+const printError = (error: any) => { console.log(error.sqlMessage || error.message) }
+
+const createTables = () => connection
+   .raw(`
+
+      CREATE TABLE IF NOT EXISTS aula_webservices_users (
+         id VARCHAR(255) PRIMARY KEY,
+         name VARCHAR(255) NOT NULL,
+         nickname VARCHAR(255) NOT NULL,
+         email VARCHAR(255) UNIQUE NOT NULL,
+         address VARCHAR(255) NOT NULL
+      );
+   `)
+   .then(() => { console.log("Tabelas criadas") })
+   .catch(printError)
+
+const closeConnection = () => { connection.destroy() }
+
+createTables()
+  
+   .finally(closeConnection)
